9a33cd8aa9f31ec6707dc5f62218e74dd1c78b4f
Marketing Director <nicolestark1103@gmail.com>
2023-10-16 11:21:26 -0500

    rules added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -51,7 +51,28 @@ module.exports = () => {
     module: {
       rules: [
         {
-          
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: "defaults" }]
+              ],
+              plugins: [
+                '@babel/plugin/proposal-object-rest-spread',
+                '@babel/transform-runtime'
+              ]
+            }
+          }
         }
         
       ],
